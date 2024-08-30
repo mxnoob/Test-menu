bc35ab00373baffc88ab00e124c9b73bba08bd5a
mxnoob <noobmx@gmail.com>
2024-08-30 11:38:12 +0300

    add TabularInline

--- menu/admin.py
+++ menu/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import MenuItem
 
 
+class MenuInline(admin.TabularInline):
+    model = MenuItem
+    extra = 1
+
+
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'menu_name', 'parent', 'url', 'named_url')
     list_filter = ('menu_name',)
+    inlines = [MenuInline]
